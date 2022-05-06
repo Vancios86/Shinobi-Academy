aa8e25ad4f194bf88f236ef0f1c103597135ecf9
Ion Albu <ialbu.webdev@gmail.com>
2022-05-06 11:36:24 +0100

    main page demo idea

---
+++ shinobi-frontend/src/Main.js
@@ -0,0 +1,20 @@
+import './Main.css';
+
+const Main = () => {
+  return (
+    <div className='Main'>
+      <div className='logo'>
+        <div className='menu'>
+        <div class='circle deg-0'>Classes</div>
+          <div class='circle deg-45'>About</div>
+          <div class='circle deg-135'>Camps</div>
+          <div class='circle deg-180'>Gallery</div>
+          <div class='circle deg-225'>Something</div>
+          <div class='circle deg-270'>Something2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
