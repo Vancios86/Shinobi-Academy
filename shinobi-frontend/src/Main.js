340fa0e756ebf03fcbbd7477fd8de2d91bb89383
Ion Albu <ialbu.webdev@gmail.com>
2022-05-13 16:41:53 +0100

    hard css reset; add utility classe; main page schema

--- shinobi-frontend/src/Main.js
+++ shinobi-frontend/src/Main.js
@@ -1,16 +1,20 @@
 import './Main.css';
+import logo from './logoB.jpeg';
 
 const Main = () => {
   return (
-    <div className='Main'>
+    <div className='main container flex'>
       <div className='logo'>
-        <div className='menu'>
-        <div class='circle deg-0'>Classes</div>
-          <div class='circle deg-45'>About</div>
-          <div class='circle deg-135'>Camps</div>
-          <div class='circle deg-180'>Gallery</div>
-          <div class='circle deg-225'>Something</div>
-          <div class='circle deg-270'>Something2</div>
+        <img src={logo} alt='' />
+      </div>
+      <div className='content'>
+        <h2 className='title flex'>SHINOBI</h2>
+        <p>Cum explicabo dolore sequi molestias fugit ratione debitis quibusdam rem, fuga consectetur dicta itaque numquam labore nulla doloribus molestiae assumenda eos similique.</p>
+        <div className="content">
+            <div className="container flex">
+              <h3 className="content media flex">fallback</h3>
+              <button className="button enter-button">ENTER</button>
+            </div>
         </div>
       </div>
     </div>
