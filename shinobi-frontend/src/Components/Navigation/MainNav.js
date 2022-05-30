e8ac5e98075cbdab7740dbae7ba51aef6b75eda0
Ion Albu <ialbu.webdev@gmail.com>
2022-05-30 11:29:52 +0100

    main navigation component

---
+++ shinobi-frontend/src/Components/Navigation/MainNav.js
@@ -0,0 +1,38 @@
+import './MainNav.css';
+import { Link } from 'react-router-dom';
+
+const MainNav = () => {
+  return (
+    <nav className='menu'>
+      <input className='menu-toggler' type='checkbox' />
+      <label
+        htmlFor='menu-toggler'
+        className='menu-toggler-label uppercase text-light fs-600'
+      >
+        MENU
+      </label>
+      <ul>
+        <li className='menu-item'>
+          <Link to='/Camps'>Camps</Link>
+        </li>
+        <li className='menu-item'>
+          <Link to='/About'>About</Link>
+        </li>
+        <li className='menu-item'>
+          <Link to='/Classes'>Classes</Link>
+        </li>
+        <li className='menu-item'>
+          <Link to='/Schedule'>Schedule</Link>
+        </li>
+        <li className='menu-item'>
+          <Link to='/Gallery'>Gallery</Link>
+        </li>
+        <li className='menu-item'>
+          <Link to='/Blog'>Blog</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MainNav;
