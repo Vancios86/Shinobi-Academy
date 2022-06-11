76d3e290254e3b571ce4859975e69b91a50d5307
Ion Albu <ialbu.webdev@gmail.com>
2022-06-11 18:44:11 +0100

    improve responsivness+components design

--- shinobi-frontend/src/Components/ClassesPage/ClassesPage.js
+++ shinobi-frontend/src/Components/ClassesPage/ClassesPage.js
@@ -1,30 +1,44 @@
 import './ClassesPage.css';
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 
 const ClassesPage = () => {
   return (
-    <div className='classes-page grid'>
-      <div className='classes-page-content'>
-        <section className='container flow'>
-          <div className='page-title' id='classes-page'>
-            <h3>Classes</h3>
-          </div>
-          <div className='grid fs-600 text-silver'>
-            <div className='classes class-jj'>
-              <h3>JiuJitsu</h3>
+    <ParallaxProvider>
+      <div className='classes-page grid'>
+        <div className='classes-page-content'>
+          <section className='container flow'>
+            <Parallax speed={-18} opacity={[1, 0]}>
+              <div className='page-title' id='classes-page'>
+                <h3>Classes</h3>
+              </div>
+            </Parallax>
+
+            <div className='grid fs-600 text-silver'>
+              <Parallax speed={5} opacity={[1, 0.77]}>
+                <div className='classes class-jj'>
+                  <h3>JiuJitsu</h3>
+                </div>
+              </Parallax>
+              <Parallax speed={0} opacity={[1, 0.77]}>
+                <div className='classes class-s'>
+                  <h3>Striking</h3>
+                </div>
+              </Parallax>
+              <Parallax speed={5} opacity={[1, 0.77]}>
+                <div className='classes class-w'>
+                  <h3>MMA Positional</h3>
+                </div>
+              </Parallax>
+              <Parallax speed={0} opacity={[1, 0.77]}>
+                <div className='classes class-spar'>
+                  <h3>Sparring</h3>
+                </div>
+              </Parallax>
             </div>
-            <div className='classes class-s'>
-              <h3>Striking</h3>
-            </div>
-            <div className='classes class-w'>
-              <h3>MMA Positional</h3>
-            </div>
-            <div className='classes class-spar'>
-              <h3>Sparring</h3>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
+    </ParallaxProvider>
   );
 };
 
--- shinobi-frontend/src/Components/WelcomePage/WelcomePage.js
+++ shinobi-frontend/src/Components/WelcomePage/WelcomePage.js
@@ -1,25 +1,28 @@
 import './WelcomePage.css';
 import MainNav from '../Navigation/MainNav';
 import Logo from '../Logo/Logo';
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 
 const WelcomePage = () => {
   return (
-    <div className='welcome-page'>
-      <div className='welcome-content'>
-        <div className='welcome-logo flex'>
-          <Logo />
-        </div>
-
-        <div className='page-title' id='welcome-text'>
-          <h3>Welcome to Shinobi Academy</h3>
-        </div>
-
-        <div className='flex nav-container'>
-          <video className='media-content flex'>fallback</video>
-          <MainNav />
+    <ParallaxProvider>
+      <div className='welcome-page'>
+        <div className='welcome-content'>
+          <div className='welcome-logo flex'>
+            <Logo />
+          </div>
+          <Parallax speed={-30} opacity={[1, 0]}>
+            <div className='page-title' id='welcome-text'>
+              <h3>Welcome to Shinobi Academy</h3>
+            </div>
+          </Parallax>
+          <div className='flex nav-container'>
+            <video className='media-content flex'>fallback</video>
+            <MainNav />
+          </div>
         </div>
       </div>
-    </div>
+    </ParallaxProvider>
   );
 };
 
