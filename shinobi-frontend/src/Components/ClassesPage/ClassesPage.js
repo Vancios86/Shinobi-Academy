5a20596bc4e636e2d3e076e9727822e76cdafcf3
Ion Albu <ialbu.webdev@gmail.com>
2022-06-13 14:01:18 +0100

    new parallax text element

--- shinobi-frontend/src/Components/ClassesPage/ClassesPage.js
+++ shinobi-frontend/src/Components/ClassesPage/ClassesPage.js
@@ -16,22 +16,63 @@ const ClassesPage = () => {
             <div className='grid fs-600 text-silver'>
               <Parallax speed={5} opacity={[1, 0.77]}>
                 <div className='classes class-jj'>
-                  <h3>JiuJitsu</h3>
+                  <div className='classes-name'>
+                    <h3>JiuJitsu</h3>
+                  </div>
+
+                  <Parallax speed={-18} opacity={[1]}>
+                    <p className='classes-text-right'>
+                      our classes Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Corporis, nesciunt totam nostrum
+                      provident quis placeat minus sunt architecto assumenda
+                      consequuntur?
+                    </p>
+                  </Parallax>
                 </div>
               </Parallax>
               <Parallax speed={0} opacity={[1, 0.77]}>
                 <div className='classes class-s'>
-                  <h3>Striking</h3>
+                  <div className='classes-name'>
+                    <h3>Striking</h3>
+                  </div>
+                  <Parallax speed={-18} opacity={[1]}>
+                    <p className='classes-text-left'>
+                      our classes Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Corporis, nesciunt totam nostrum
+                      provident quis placeat minus sunt architecto assumenda
+                      consequuntur?
+                    </p>
+                  </Parallax>
                 </div>
               </Parallax>
               <Parallax speed={5} opacity={[1, 0.77]}>
                 <div className='classes class-w'>
-                  <h3>MMA Positional</h3>
+                  <div className='classes-name'>
+                    <h3>MMA Positional</h3>
+                  </div>
+                  <Parallax speed={-18} opacity={[1]}>
+                    <p className='classes-text-right'>
+                      our classes Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Corporis, nesciunt totam nostrum
+                      provident quis placeat minus sunt architecto assumenda
+                      consequuntur?
+                    </p>
+                  </Parallax>
                 </div>
               </Parallax>
               <Parallax speed={0} opacity={[1, 0.77]}>
                 <div className='classes class-spar'>
-                  <h3>Sparring</h3>
+                  <div className='classes-name'>
+                    <h3>Sparring</h3>
+                  </div>
+                  <Parallax speed={-18} opacity={[1]}>
+                    <p className='classes-text-left'>
+                      our classes Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Corporis, nesciunt totam nostrum
+                      provident quis placeat minus sunt architecto assumenda
+                      consequuntur?
+                    </p>
+                  </Parallax>
                 </div>
               </Parallax>
             </div>
