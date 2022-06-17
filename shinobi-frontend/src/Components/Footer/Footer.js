4c3202afb265b4eb1c79b9e0599adfb20c1db8f8
Ion Albu <ialbu.webdev@gmail.com>
2022-06-17 16:16:15 +0100

    change footer styling, make it responsive

--- shinobi-frontend/src/Components/Footer/Footer.js
+++ shinobi-frontend/src/Components/Footer/Footer.js
@@ -1,49 +1,61 @@
 import './Footer.css';
 import { MdPhone, MdEmail, MdLocationPin } from 'react-icons/md';
-import { SiInstagram, SiTwitter, SiYoutube } from 'react-icons/si';
+import { SiTwitter, SiYoutube } from 'react-icons/si';
 import { IconContext } from 'react-icons';
 import vector from '../Logo/vector.jpeg';
 import Tilt from 'react-parallax-tilt';
+import { AiFillInstagram } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div className='footer container flex bg-light text-dark fs300'>
-      <div className='page-title' id='contact-page'>
-        <h3>Contact</h3>
-      </div>
-      <IconContext.Provider
-        value={{ color: 'hsl(var(--clr-red))', className: 'global-class-name' }}
-      >
-        <div className='contact flex'>
-          <ul>
-            <li>
-              <span>
-                <MdPhone />
-              </span>{' '}
-              077777777
-            </li>
-            <li>
-              <MdEmail /> colinbyrne@mail.com
-            </li>
-            <li>
-              <MdLocationPin /> Rua xxxxx nr15
-            </li>
-          </ul>
+    <div className='footer container grid bg-light text-dark'>
+      <div className='section footer-top'>
+        <div className='logo-footer'>
+          <Tilt>
+            <img src={vector} alt='vector-logo' />
+          </Tilt>
         </div>
-      </IconContext.Provider>
 
-      <div className='logo-footer'>
-        <Tilt>
-          <img src={vector} alt='vector-logo' />
-        </Tilt>
+        <IconContext.Provider
+          value={{
+            color: 'hsl(var(--clr-red))',
+            className: 'global-class-name',
+          }}
+        >
+          <div className='contact flex'>
+            <ul>
+              <li>
+                <span>
+                  <MdPhone />
+                </span>{' '}
+                977 777 777
+              </li>
+              <li>
+                <MdEmail /> shinobyacademy@gmail.com
+              </li>
+              <li>
+                <MdLocationPin /> Rua xxxxx nr15
+              </li>
+            </ul>
+          </div>
+        </IconContext.Provider>
       </div>
+      {/* <div className='map'></div> */}
 
-      <div className='contact-form'>
+      {/* <div className='contact-form'>
+        <div className='contact-form-title'>Send us a message</div>
         <form action=''>
           <input type='text' name='name' id='name' placeholder='your name' />
           <br />
-          <input type='text' name='email' id='email' placeholder='email' />
-
+          <input type='email' name='email' id='email' placeholder='email' />
+          <br />
+          <textarea
+            type='text'
+            name='message'
+            id='message'
+            placeholder='your message'
+          />
+          <br />
           <input
             type='button'
             name='send-message'
@@ -51,7 +63,7 @@ const Footer = () => {
             value='send'
           />
         </form>
-      </div>
+      </div> */}
 
       <IconContext.Provider
         value={{
@@ -60,18 +72,20 @@ const Footer = () => {
           className: 'social-media-icons',
         }}
       >
-        <div className='social-media clr-red'>
-          <ul className='flex'>
-            <li>
-              <SiInstagram />
-            </li>
-            <li>
-              <SiTwitter />
-            </li>
-            <li>
-              <SiYoutube />
-            </li>
-          </ul>
+        <div className='section footer-bottom'>
+          <div className='social-media clr-red'>
+            <ul className='flex'>
+              <li>
+                <AiFillInstagram />
+              </li>
+              <li>
+                <SiTwitter />
+              </li>
+              <li>
+                <SiYoutube />
+              </li>
+            </ul>
+          </div>
         </div>
       </IconContext.Provider>
     </div>
