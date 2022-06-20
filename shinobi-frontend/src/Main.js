bf30d825a649acd21be1d623b8973532d8c2d22a
Ion Albu <ialbu.webdev@gmail.com>
2022-06-20 18:09:27 +0100

    group the components in a separate div, control responsive behaviour

--- shinobi-frontend/src/Main.js
+++ shinobi-frontend/src/Main.js
@@ -4,11 +4,13 @@ import Footer from './Components/Footer/Footer';
 import WelcomePage from './Components/WelcomePage/WelcomePage';
 import AboutPage from './Components/AboutPage/AboutPage';
 import ClassesPage from './Components/ClassesPage/ClassesPage';
+import CampsComponent from './Components/CampsComponent/CampsComponent';
+import ContactComponent from './Components/Contact/ContactComponent';
 import { Routes, Route } from 'react-router-dom';
 
 const Main = () => {
   return (
-    <div className='main grid'>
+    <div className='main grid container'>
       {/* <Routes>
         <Route path='/' element={<WelcomePage />} />
         <Route path='/#about-page' element={<AboutPage />} />
@@ -16,6 +18,10 @@ const Main = () => {
       <WelcomePage />
       <AboutPage />
       <ClassesPage />
+      <div className='components-section'>
+        <CampsComponent />
+        <ContactComponent />
+      </div>
       <Footer />
     </div>
   );
