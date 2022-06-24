db7509b7e66261d0166badda98fb31ca37c5b412
Ion Albu <ialbu.webdev@gmail.com>
2022-06-24 20:34:12 +0100

    add parallax to wrap the whole component

--- shinobi-frontend/src/Main.js
+++ shinobi-frontend/src/Main.js
@@ -5,24 +5,30 @@ import WelcomePage from './Components/WelcomePage/WelcomePage';
 import AboutPage from './Components/AboutPage/AboutPage';
 import ClassesPage from './Components/ClassesPage/ClassesPage';
 import CampsComponent from './Components/CampsComponent/CampsComponent';
-import ContactComponent from './Components/Contact/ContactComponent';
-import { Routes, Route } from 'react-router-dom';
+import GalleryComponent from './Components/Gallery/GalleryComponent';
+import ContactPage from './Components/Contact/ContactPage';
+// import { Routes, Route } from 'react-router-dom';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 const Main = () => {
   return (
     <div className='main grid container'>
-      {/* <Routes>
+      <ParallaxProvider>
+        {/* <Routes>
         <Route path='/' element={<WelcomePage />} />
         <Route path='/#about-page' element={<AboutPage />} />
       </Routes> */}
-      <WelcomePage />
-      <AboutPage />
-      <ClassesPage />
-      <div className='components-section'>
-        <CampsComponent />
-        <ContactComponent />
-      </div>
-      <Footer />
+        <WelcomePage />
+        <AboutPage />
+        <ClassesPage />
+
+        <div className='inline-components-section'>
+          <CampsComponent />
+          <GalleryComponent />
+        </div>
+        <ContactPage />
+        <Footer />
+      </ParallaxProvider>
     </div>
   );
 };
