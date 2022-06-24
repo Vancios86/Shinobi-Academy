bb4b727d55910fa71f0b3f401ded4a93a041ce38
Ion Albu <ialbu.webdev@gmail.com>
2022-06-24 20:22:08 +0100

    render team members dynamically, improve styling

--- shinobi-frontend/src/Components/AboutPage/AboutPage.js
+++ shinobi-frontend/src/Components/AboutPage/AboutPage.js
@@ -1,124 +1,87 @@
 import './AboutPage.css';
 import Tilt from 'react-parallax-tilt';
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax } from 'react-scroll-parallax';
+import data from '../../data.json';
+
+const teamMemberCard = (id, name, xp, skills) => {
+  return (
+    <Tilt
+      className='parallax-effect-glare-scale'
+      perspective={500}
+      glareEnable={true}
+      glareMaxOpacity={0.5}
+      scale={1.02}
+      key={id}
+    >
+      <div className='team-member'>
+        <div className='coach-photo photo-colin-coach'></div>
+        <p className='coach-description'>
+          Name: {name} <br /> Experience: {xp} <br />
+          Skills: {skills}
+        </p>
+      </div>
+    </Tilt>
+  );
+};
 
 const AboutPage = () => {
   return (
-    <ParallaxProvider>
-      <div className='about-page grid'>
-        <div className='page-title' id='about-page'>
-          <h3>About us</h3>
-        </div>
-        <Parallax speed={16} opacity={[1]}>
-          <div className='about-page-content'>
-            <section className='container flow'>
-              <div className='container'>
-                <div className='photo-colin'>
-                  <Tilt
-                    className='parallax-effect-glare-scale'
-                    perspective={500}
-                    glareEnable={true}
-                    glareMaxOpacity={0.5}
-                    scale={1.01}
-                  >
-                    <img src='https://source.unsplash.com/cVLOqm8sSXc' alt='' />
-                  </Tilt>
-                </div>
-
-                <p className='text-dark text-content' id='about-text'>
-                  This is Colin Byrne, the founder of Shinobi Academy. Labore
-                  consequuntur officia adipisci, dolorum et dolorem neque
-                  explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
-                  recusandae voluptatem error quisquam ducimus obcaecati. Lorem
-                  ipsum dolor sit amet consectetur adipisicing elit. Labore
-                  consequuntur officia adipisci, dolorum et dolorem neque
-                  explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
-                  recusandae voluptatem error quisquam ducimus obcaecati. Lorem
-                  ipsum dolor sit amet consectetur adipisicing elit. Labore
-                  consequuntur officia adipisci, dolorum et dolorem neque
-                  explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
-                  recusandae voluptatem error quisquam ducimus obcaecati.
-                  Doloremque maxime quibusdam, recusandae voluptatem error
-                  quisquam ducimus obcaecati.Doloremque maxime quibusdam,
-                  recusandae voluptatem error quisquam ducimus
-                  obcaecati.Doloremque maxime quibusdam, recusandae voluptatem
-                  error quisquam ducimus obcaecati.
-                </p>
-              </div>
-            </section>
-
-            <section className='container flow'>
-              <div className='team-title'>
-                <h3>MEET OUR TEAM</h3>
-              </div>
-
-              <div className='coaches-wrapper container grid'>
-                <Tilt
-                  className='parallax-effect-glare-scale'
-                  perspective={500}
-                  glareEnable={true}
-                  glareMaxOpacity={0.5}
-                  scale={1.02}
-                >
-                  <div className='team-member'>
-                    <div className='coach-photo photo-colin-coach'></div>
-                    <p className='coach-description'>
-                      Name: Colin <br /> Experience: 20+years <br />
-                      Skills: pro fighter, BJJ, Ninjutsu BlackBelt
-                    </p>
-                  </div>
-                </Tilt>
-                <Tilt
-                  className='parallax-effect-glare-scale'
-                  perspective={500}
-                  glareEnable={true}
-                  glareMaxOpacity={0.5}
-                  scale={1.02}
-                >
-                  <div className='team-member'>
-                    <div className='coach-photo photo-kieran'></div>
-                    <p className='coach-description'>
-                      Name: Kieran <br /> Experience: 10years <br />
-                      Skills: pro MMA fighter, BJJ purple belt
-                    </p>
-                  </div>
-                </Tilt>
-                <Tilt
-                  className='parallax-effect-glare-scale'
-                  perspective={500}
-                  glareEnable={true}
-                  glareMaxOpacity={0.5}
-                  scale={1.02}
-                >
-                  <div className='team-member'>
-                    <div className='coach-photo photo-josh'></div>
-                    <p className='coach-description'>
-                      Name: Josh <br /> Experience: 20years <br /> Skills: BJJ
-                      black belt, multiple European podium finisher
-                    </p>
-                  </div>
-                </Tilt>
-                <Tilt
-                  className='parallax-effect-glare-scale'
-                  perspective={500}
-                  glareEnable={true}
-                  glareMaxOpacity={0.5}
-                  scale={1.02}
-                >
-                  <div className='team-member'>
-                    <div className='coach-photo photo-bryne'></div>
-                    <p className='coach-description'>
-                      Name: Bryne <br /> Experience: 20years <br /> Skills: pro
-                      fighter, wrestling, boxing
-                    </p>
-                  </div>
-                </Tilt>
-              </div>
-            </section>
-          </div>
-        </Parallax>
+    <div className='about-page grid'>
+      <div className='page-title' id='about-page'>
+        <h3>About us</h3>
       </div>
-    </ParallaxProvider>
+      <Parallax speed={16} opacity={[1]}>
+        <div className='about-page-content'>
+          <section className='container flow'>
+            <div className='container'>
+              <div className='photo-colin'>
+                <Tilt
+                  className='parallax-effect-glare-scale'
+                  perspective={500}
+                  glareEnable={true}
+                  glareMaxOpacity={0.5}
+                  scale={1.01}
+                >
+                  <img src='https://source.unsplash.com/cVLOqm8sSXc' alt='' />
+                </Tilt>
+              </div>
+
+              <p className='text-dark text-content' id='about-text'>
+                This is Colin Byrne, the founder of Shinobi Academy. Labore
+                consequuntur officia adipisci, dolorum et dolorem neque
+                explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
+                recusandae voluptatem error quisquam ducimus obcaecati. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Labore
+                consequuntur officia adipisci, dolorum et dolorem neque
+                explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
+                recusandae voluptatem error quisquam ducimus obcaecati. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Labore
+                consequuntur officia adipisci, dolorum et dolorem neque
+                explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
+                recusandae voluptatem error quisquam ducimus obcaecati.
+                Doloremque maxime quibusdam, recusandae voluptatem error
+                quisquam ducimus obcaecati.Doloremque maxime quibusdam,
+                recusandae voluptatem error quisquam ducimus
+                obcaecati.Doloremque maxime quibusdam, recusandae voluptatem
+                error quisquam ducimus obcaecati.
+              </p>
+            </div>
+          </section>
+
+          <section className='container flow'>
+            <div className='team-title'>
+              <h3>MEET OUR TEAM</h3>
+            </div>
+
+            <div className='coaches-wrapper container grid'>
+              {data.teamMembers.map((member) =>
+                teamMemberCard(member.id, member.name, member.xp, member.skills)
+              )}
+            </div>
+          </section>
+        </div>
+      </Parallax>
+    </div>
   );
 };
 
