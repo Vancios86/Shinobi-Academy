810de5a22c88d7df3bbebd1919f3dce306124d36
Ion Albu <ialbu.webdev@gmail.com>
2022-06-28 16:34:30 +0100

    delete unneeded code

--- shinobi-frontend/src/Main.js
+++ shinobi-frontend/src/Main.js
@@ -4,24 +4,18 @@ import Footer from './Components/Footer/Footer';
 import WelcomePage from './Components/WelcomePage/WelcomePage';
 import AboutPage from './Components/AboutPage/AboutPage';
 import ClassesPage from './Components/ClassesPage/ClassesPage';
-import CampsComponent from './Components/CampsComponent/CampsComponent';
-import GalleryComponent from './Components/Gallery/GalleryComponent';
+import CampsComponent from './Components/Camps/CampsComponent/CampsComponent';
+import GalleryComponent from './Components/Gallery/GalleryComponent/GalleryComponent';
 import ContactPage from './Components/Contact/ContactPage';
-// import { Routes, Route } from 'react-router-dom';
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 const Main = () => {
   return (
     <div className='main grid container'>
       <ParallaxProvider>
-        {/* <Routes>
-        <Route path='/' element={<WelcomePage />} />
-        <Route path='/#about-page' element={<AboutPage />} />
-      </Routes> */}
         <WelcomePage />
         <AboutPage />
         <ClassesPage />
-
         <div className='inline-components-section'>
           <CampsComponent />
           <GalleryComponent />
