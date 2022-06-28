3cb1f6d002dfb9ad75c291d69ffdfcec64ed9696
Ion Albu <ialbu.webdev@gmail.com>
2022-06-28 11:29:32 +0100

    add contact page draft

---
+++ shinobi-frontend/src/Components/Contact/ContactPage.js
@@ -0,0 +1,76 @@
+import './ContactPage.css';
+import ContactFormComponent from '../ContactFormComponent/ContactFormComponent';
+import { Parallax } from 'react-scroll-parallax';
+import { MdPhone, MdEmail, MdLocationPin } from 'react-icons/md';
+import { IconContext } from 'react-icons';
+import { SiTwitter, SiYoutube } from 'react-icons/si';
+import { AiFillInstagram } from 'react-icons/ai';
+
+const ContactPage = () => {
+  return (
+    <Parallax speed={3}>
+      <div className='contact-page container flex'>
+        <div className='page-title'>
+          <h3>Contact</h3>
+        </div>
+
+        <div className='inline-elements inline-elements-top'>
+          <IconContext.Provider
+            value={{
+              color: 'hsl(var(--clr-red))',
+              size: '2.25em',
+              className: 'social-media-icons',
+            }}
+          >
+            <div className='social-media-container shadowed-box flex clr-red'>
+              <div className='social-media '>
+                <ul className='flex'>
+                  <li className='sm-item'>
+                    <AiFillInstagram />
+                  </li>
+                  <li className='sm-item'>
+                    <SiTwitter />
+                  </li>
+                  <li className='sm-item'>
+                    <SiYoutube />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </IconContext.Provider>
+
+          <IconContext.Provider
+            value={{
+              color: 'hsl(var(--clr-red))',
+              className: 'global-class-name',
+              size: '21px',
+            }}
+          >
+            <div className='contact-list-container flex'>
+              <div className='contact-list flex'>
+                <ul className='shadowed-box contact-list-items flex'>
+                  <li>
+                    <MdPhone /> 977 777 777
+                  </li>
+                  <li>
+                    <MdEmail /> shinobyacademy@gmail.com
+                  </li>
+                  <li>
+                    <MdLocationPin /> Iberlagos Rua Don Jose nr15
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </IconContext.Provider>
+        </div>
+
+        <div className='inline-elements inline-elements-bottom'>
+          <div className='map shadowed-box'>MAP</div>
+          <ContactFormComponent />
+        </div>
+      </div>
+    </Parallax>
+  );
+};
+
+export default ContactPage;
