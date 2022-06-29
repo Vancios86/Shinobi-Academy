775131f7d6f0f778b90f3fe22e2ba780268be4ba
Ion Albu <ialbu.webdev@gmail.com>
2022-06-29 17:31:30 +0100

    improve schedule page draft

--- shinobi-frontend/src/Components/ClassesPage/SchedulePage/SchedulePage.js
+++ shinobi-frontend/src/Components/ClassesPage/SchedulePage/SchedulePage.js
@@ -8,11 +8,16 @@ const SchedulePage = () => {
       <div className='welcome-logo flex'>
         <Logo />
       </div>
+      <div className='back-button'>
+        <button>&#11013;</button>
+      </div>
       <div className='schedule-page-content'>
         <div className='page-title'>
           <h3>Weekly Schedule</h3>
         </div>
-        <div className='schedule-container container flow'></div>
+        <div className='schedule-container flow flex'>
+          <h3>Here will be displayed the schedule</h3>
+        </div>
       </div>
       <div className='secondary-page-footer'>
         <Footer />
