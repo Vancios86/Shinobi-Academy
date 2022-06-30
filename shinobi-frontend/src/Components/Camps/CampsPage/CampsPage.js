2ddba02a115e00c6da7ae334d86adab5630e0038
Ion Albu <ialbu.webdev@gmail.com>
2022-06-30 14:07:24 +0100

    insert scroll to top on component mount, navigate back button, modify the draft

--- shinobi-frontend/src/Components/Camps/CampsPage/CampsPage.js
+++ shinobi-frontend/src/Components/Camps/CampsPage/CampsPage.js
@@ -1,34 +1,41 @@
 import './CampsPage.css';
 import Logo from '../../Logo/Logo';
 import Footer from '../../Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const CampsPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log(
+      'X position :',
+      window.scrollX,
+      ' |Y position: ',
+      window.scrollY
+    );
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
   return (
     <div className='camps-page container'>
       <div className='welcome-logo flex'>
         <Logo />
       </div>
+      <div className='back-button'>
+        <button onClick={() => navigate(-1)}>&#11013;</button>
+      </div>
       <div className='camps-page-content'>
         <div className='page-title'>
           <h3>Camps</h3>
         </div>
-        <div className='camps-description container flow'>
-          <p className='text-content'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            saepe quae autem, aperiam pariatur voluptatibus blanditiis sapiente
-            vel ut magnam velit ea repellat libero officiis impedit iste illo
-            dolorum consectetur eum! Omnis ipsum qui ipsa commodi necessitatibus
-            quod maxime. Minima libero maxime veritatis dicta. Ad repellat quas
-            quibusdam tenetur animi iusto odit harum voluptatum recusandae!
-            Ullam, repellendus! Sit vero labore dolorum nemo debitis, rerum
-            asperiores eum officia. Earum vitae quasi accusantium itaque rem
-            non, soluta distinctio est quia veritatis, expedita nobis odit
-            dolore beatae libero, molestiae sint in reiciendis! Distinctio
-            exercitationem sunt dolorum rerum repudiandae in vero at dolorem
-            pariatur.
-          </p>
-          <div className='camps-photo'></div>
+
+        <div className='schedule-container flow flex'>
+          <h3>Here we'll have information about the camps and media files</h3>
         </div>
+
+        <div className='camps-photo'></div>
       </div>
       <div className='secondary-page-footer'>
         <Footer />
