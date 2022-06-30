7e7c64987bfa2e660d3fb51e2dcf57637bc511bd
Ion Albu <ialbu.webdev@gmail.com>
2022-06-30 14:08:52 +0100

    insert scroll to top on component mount, navigate back button, modify the draft

--- shinobi-frontend/src/Components/ClassesPage/SchedulePage/SchedulePage.js
+++ shinobi-frontend/src/Components/ClassesPage/SchedulePage/SchedulePage.js
@@ -1,15 +1,30 @@
 import './SchedulePage.css';
 import Logo from '../../Logo/Logo';
 import Footer from '../../Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const SchedulePage = () => {
+  useEffect(() => {
+    console.log(
+      'X position :',
+      window.scrollX,
+      ' |Y position: ',
+      window.scrollY
+    );
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+  const navigate = useNavigate();
   return (
     <div className='schedule-page container'>
       <div className='welcome-logo flex'>
         <Logo />
       </div>
       <div className='back-button'>
-        <button>&#11013;</button>
+        <button onClick={() => navigate(-1)}>&#11013;</button>
       </div>
       <div className='schedule-page-content'>
         <div className='page-title'>
