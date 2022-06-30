0522576648530f49d462089949d86ff8d0c84548
Ion Albu <ialbu.webdev@gmail.com>
2022-06-30 14:10:53 +0100

    insert scroll to top on component mount, navigate back button, modify the draft

--- shinobi-frontend/src/Components/Gallery/GalleryPage/GalleryPage.js
+++ shinobi-frontend/src/Components/Gallery/GalleryPage/GalleryPage.js
@@ -1,18 +1,38 @@
 import './GalleryPage.css';
 import Logo from '../../Logo/Logo';
 import Footer from '../../Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const GalleryPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log(
+      'X position :',
+      window.scrollX,
+      ' |Y position: ',
+      window.scrollY
+    );
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
   return (
     <div className='gallery-page container'>
       <div className='welcome-logo flex'>
         <Logo />
       </div>
+      <div className='back-button'>
+        <button onClick={() => navigate(-1)}>&#11013;</button>
+      </div>
       <div className='gallery-page-content'>
         <div className='page-title'>
           <h3>Media Gallery</h3>
         </div>
-        <div className='gallery-container container flow'></div>
+        <div className='schedule-container flow flex'>
+          <h3>Here we'll load the media files for the gallery</h3>
+        </div>
       </div>
       <div className='secondary-page-footer'>
         <Footer />
