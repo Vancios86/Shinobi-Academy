63a4d82884c25d3cdf65398aecced3b60f9f8f96
Ion Albu <ialbu.webdev@gmail.com>
2022-07-03 15:45:07 +0100

    add back-to-top button and functionality

--- shinobi-frontend/src/Main.js
+++ shinobi-frontend/src/Main.js
@@ -8,8 +8,19 @@ import CampsComponent from './Components/Camps/CampsComponent/CampsComponent';
 import GalleryComponent from './Components/Gallery/GalleryComponent/GalleryComponent';
 import ContactPage from './Components/Contact/ContactPage';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { useEffect } from 'react';
 
 const Main = () => {
+  useEffect(() => {
+    var link = document.querySelector('.go-to-top-button');
+
+    window.addEventListener('scroll', (e) => {
+      window.pageYOffset > 1100
+        ? link.classList.add('show')
+        : (link.className = 'go-to-top-button');
+    });
+  });
+
   return (
     <div className='main grid container'>
       <ParallaxProvider>
@@ -23,6 +34,12 @@ const Main = () => {
         <ContactPage />
         <Footer />
       </ParallaxProvider>
+
+      <button className='go-to-top-button'>
+        <a href='#main-video' id='back-to-top-link'>
+          <b>↟</b>
+        </a>
+      </button>
     </div>
   );
 };
