48057e815717cc89976efcd9873fe299b883f369
Ion Albu <ialbu.webdev@gmail.com>
2022-08-14 15:46:52 +0100

    fix navigation links

--- shinobi-frontend/src/Components/Navigation/Navigation.js
+++ shinobi-frontend/src/Components/Navigation/Navigation.js
@@ -3,29 +3,32 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className='menu'>
-      <input className='menu-toggler' type='checkbox' />
-      <label htmlFor='menu-toggler' className='menu-toggler-label'>
-        <span className='Navigation-icon'>&nbsp;</span>
+    <nav className="menu">
+      <input className="menu-toggler" type="checkbox" />
+      <label
+        htmlFor="menu-toggler"
+        className="menu-toggler-label border-dark shadowed-box"
+      >
+        <span className="Navigation-icon">&nbsp;</span>
       </label>
       <ul>
-        <li className='menu-item'>
-          <Link to='/Camps'>Camps </Link>
+        <li className="menu-item">
+          <Link to="/camps">Camps </Link>
         </li>
-        <li className='menu-item'>
-          <a href='/#about-page'>About</a>
+        <li className="menu-item">
+          <a href="#about-page">About</a>
         </li>
-        <li className='menu-item'>
-          <a href='/#classes-page'>Classes</a>
+        <li className="menu-item">
+          <a href="#classes-page">Classes</a>
         </li>
-        <li className='menu-item'>
-          <Link to='/schedule'>Schedule</Link>
+        <li className="menu-item">
+          <Link to="/schedule">Schedule</Link>
         </li>
-        <li className='menu-item'>
-          <Link to='/gallery'>Gallery</Link>
+        <li className="menu-item">
+          <Link to="/gallery">Gallery</Link>
         </li>
-        <li className='menu-item'>
-          <a href='/#contact'>Contact</a>
+        <li className="menu-item">
+          <a href="#contact">Contact</a>
         </li>
       </ul>
     </nav>
