7667d821dd53343e5dc8ad791c9ffdd77420e0ca
Ion Albu <ialbu.webdev@gmail.com>
2022-08-14 15:44:27 +0100

    new top page

---
+++ shinobi-frontend/src/Components/TopPage/TopPage.js
@@ -0,0 +1,30 @@
+import './TopPage.css';
+import Navigation from '../Navigation/Navigation';
+import Logo from '../Logo/Logo';
+
+const TopPage = () => {
+  return (
+    <div className="top-page">
+      <div className="top-content">
+        <div className="top-logo flex">
+          <Logo />
+        </div>
+        <div className="page-title" id="welcome-text">
+          <h3 data-text="Welcome to Shinobi Academy">
+            Welcome to <span>Shinobi</span> Academy
+          </h3>
+        </div>
+        <div className="shadowed-box" id="nav-container">
+          <Navigation />
+        </div>
+        <div className="partners-logos">
+          <div className="partner1"></div>
+          <div className="partner2"></div>
+          <div className="partner3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopPage;
