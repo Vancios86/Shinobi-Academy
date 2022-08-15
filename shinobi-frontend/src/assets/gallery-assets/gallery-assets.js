f5ef72b0a54d5b86ed23826d6671324a4bf4e8bd
Ion Albu <ialbu.webdev@gmail.com>
2022-08-15 15:23:12 +0100

    add gallery assets + javascript

---
+++ shinobi-frontend/src/assets/gallery-assets/gallery-assets.js
@@ -0,0 +1,279 @@
+import img1 from './gallery-media/img1.jpg';
+import img2 from './gallery-media/img2.jpg';
+import img3 from './gallery-media/img3.jpg';
+import img4 from './gallery-media/img4.jpg';
+import img5 from './gallery-media/img5.jpg';
+import img6 from './gallery-media/img6.jpg';
+import img7 from './gallery-media/img7.jpg';
+import img8 from './gallery-media/img8.jpg';
+import img9 from './gallery-media/img9.jpg';
+import img10 from './gallery-media/img10.jpg';
+import img11 from './gallery-media/img11.jpg';
+import img12 from './gallery-media/img12.jpg';
+import img13 from './gallery-media/img13.jpg';
+import img14 from './gallery-media/img14.jpg';
+import img15 from './gallery-media/img15.jpg';
+import img16 from './gallery-media/img16.jpg';
+import img17 from './gallery-media/img17.jpg';
+import img18 from './gallery-media/img18.jpg';
+import img19 from './gallery-media/img19.jpg';
+import img20 from './gallery-media/img20.jpg';
+import img21 from './gallery-media/img21.jpg';
+import img22 from './gallery-media/img22.jpg';
+import img23 from './gallery-media/img23.jpg';
+import img24 from './gallery-media/img24.jpg';
+import img25 from './gallery-media/img25.jpg';
+import img26 from './gallery-media/img26.jpg';
+import img27 from './gallery-media/img27.jpg';
+import img28 from './gallery-media/img28.jpg';
+import img29 from './gallery-media/img29.jpg';
+import img30 from './gallery-media/img30.jpg';
+import img31 from './gallery-media/img31.jpg';
+import img32 from './gallery-media/img32.jpg';
+import img33 from './gallery-media/img33.jpg';
+import img34 from './gallery-media/img34.jpg';
+import img35 from './gallery-media/img35.jpg';
+import img36 from './gallery-media/img36.jpg';
+import img37 from './gallery-media/img37.jpg';
+import img38 from './gallery-media/img38.jpg';
+import img39 from './gallery-media/img39.jpg';
+import img40 from './gallery-media/img40.jpg';
+import img41 from './gallery-media/img41.jpg';
+import img42 from './gallery-media/img42.jpg';
+import img43 from './gallery-media/img43.jpg';
+import img44 from './gallery-media/img44.jpg';
+import img45 from './gallery-media/img45.jpg';
+import img46 from './gallery-media/img46.jpg';
+
+export const images = [
+  {
+    id: 1,
+    title: 'Image 1',
+    src: img1,
+  },
+  {
+    id: 2,
+    title: 'Image 2',
+    src: img2,
+  },
+  {
+    id: 3,
+    title: 'Image 3',
+    src: img3,
+  },
+  {
+    id: 4,
+    title: 'Image 4',
+    src: img4,
+  },
+  {
+    id: 5,
+    title: 'Image 5',
+    src: img5,
+  },
+  {
+    id: 6,
+    title: 'Image 6',
+    src: img6,
+  },
+  {
+    id: 7,
+    title: 'Image 7',
+    src: img7,
+  },
+  {
+    id: 8,
+    title: 'Image 8',
+    src: img8,
+  },
+  {
+    id: 9,
+    title: 'Image 9',
+    src: img9,
+  },
+  {
+    id: 10,
+    title: 'Image 10',
+    src: img10,
+  },
+  {
+    id: 11,
+    title: 'Image 11',
+    src: img11,
+  },
+  {
+    id: 12,
+    title: 'Image 12',
+    src: img12,
+  },
+  {
+    id: 13,
+    title: 'Image 13',
+    src: img13,
+  },
+  {
+    id: 14,
+    title: 'Image 14',
+    src: img14,
+  },
+  {
+    id: 15,
+    title: 'Image 15',
+    src: img15,
+  },
+  {
+    id: 16,
+    title: 'Image 16',
+    src: img16,
+  },
+  {
+    id: 17,
+    title: 'Image 17',
+    src: img17,
+  },
+  {
+    id: 18,
+    title: 'Image 18',
+    src: img18,
+  },
+  {
+    id: 19,
+    title: 'Image 19',
+    src: img19,
+  },
+  {
+    id: 20,
+    title: 'Image 20',
+    src: img20,
+  },
+  {
+    id: 21,
+    title: 'Image 21',
+    src: img21,
+  },
+  {
+    id: 22,
+    title: 'Image 22',
+    src: img22,
+  },
+  {
+    id: 23,
+    title: 'Image 23',
+    src: img23,
+  },
+  {
+    id: 24,
+    title: 'Image 24',
+    src: img24,
+  },
+  {
+    id: 25,
+    title: 'Image 25',
+    src: img25,
+  },
+  {
+    id: 26,
+    title: 'Image 26',
+    src: img26,
+  },
+  {
+    id: 27,
+    title: 'Image 27',
+    src: img27,
+  },
+  {
+    id: 28,
+    title: 'Image 28',
+    src: img28,
+  },
+  {
+    id: 29,
+    title: 'Image 29',
+    src: img29,
+  },
+  {
+    id: 30,
+    title: 'Image 30',
+    src: img30,
+  },
+  {
+    id: 31,
+    title: 'Image 31',
+    src: img31,
+  },
+  {
+    id: 32,
+    title: 'Image 32',
+    src: img32,
+  },
+  {
+    id: 33,
+    title: 'Image 33',
+    src: img33,
+  },
+  {
+    id: 34,
+    title: 'Image 34',
+    src: img34,
+  },
+  {
+    id: 35,
+    title: 'Image 35',
+    src: img35,
+  },
+  {
+    id: 36,
+    title: 'Image 36',
+    src: img36,
+  },
+  {
+    id: 37,
+    title: 'Image 37',
+    src: img37,
+  },
+  {
+    id: 38,
+    title: 'Image 38',
+    src: img38,
+  },
+  {
+    id: 39,
+    title: 'Image 39',
+    src: img39,
+  },
+  {
+    id: 40,
+    title: 'Image 40',
+    src: img40,
+  },
+  {
+    id: 41,
+    title: 'Image 41',
+    src: img41,
+  },
+  {
+    id: 42,
+    title: 'Image 42',
+    src: img42,
+  },
+  {
+    id: 43,
+    title: 'Image 43',
+    src: img43,
+  },
+  {
+    id: 44,
+    title: 'Image 44',
+    src: img44,
+  },
+  {
+    id: 45,
+    title: 'Image 45',
+    src: img45,
+  },
+  {
+    id: 46,
+    title: 'Image 46',
+    src: img46,
+  },
+];
