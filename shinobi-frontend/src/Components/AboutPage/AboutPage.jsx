33c54b70533daccb81e501c5aed5d41a68eef841
Ion Albu <ialbu.webdev@gmail.com>
2022-08-16 15:51:12 +0100

    add aside section to about page + styling

--- shinobi-frontend/src/Components/AboutPage/AboutPage.jsx
+++ shinobi-frontend/src/Components/AboutPage/AboutPage.jsx
@@ -1,20 +1,21 @@
 import './AboutPage.css';
 import Tilt from 'react-parallax-tilt';
 import data from '../../data.json';
+import img46 from '../../assets/gallery-assets/gallery-media/img46.jpg';
 
 const teamMemberCard = (id, name, xp, skills) => {
   return (
     <Tilt
-      className="parallax-effect-glare-scale"
+      className='parallax-effect-glare-scale'
       perspective={500}
       glareEnable={true}
       glareMaxOpacity={0.5}
       scale={1.02}
       key={id}
     >
-      <div className="team-member">
-        <div className="coach-photo photo-colin-coach"></div>
-        <p className="coach-description">
+      <div className='team-member'>
+        <div className='coach-photo photo-colin-coach'></div>
+        <p className='coach-description'>
           Name: {name} <br /> Experience: {xp} <br />
           Skills: {skills}
         </p>
@@ -25,26 +26,26 @@ const teamMemberCard = (id, name, xp, skills) => {
 
 const AboutPage = () => {
   return (
-    <div className="about-page grid">
-      <div className="page-title" id="about-page">
+    <div className='about-page grid'>
+      <div className='page-title' id='about-page'>
         <h3>About us</h3>
       </div>
-      <div className="about-page-content shadowed-box">
-        <section className="container flow">
-          <div className="container">
-            <div className="photo-colin">
+      <div className='about-page-content shadowed-box'>
+        <section className='container flow'>
+          <div className='container'>
+            <div className='photo-colin'>
               <Tilt
-                className="parallax-effect-glare-scale"
+                className='parallax-effect-glare-scale'
                 perspective={500}
                 glareEnable={true}
                 glareMaxOpacity={0.5}
                 scale={1.01}
               >
-                <img src="https://source.unsplash.com/cVLOqm8sSXc" alt="" />
+                <img src='https://source.unsplash.com/cVLOqm8sSXc' alt='' />
               </Tilt>
             </div>
 
-            <p className="text-dark text-content" id="about-text">
+            <p className='text-dark text-content' id='about-text'>
               This is Colin Byrne, the founder of Shinobi Academy. Labore
               consequuntur officia adipisci, dolorum et dolorem neque explicabo
               aut nostrum rem atque. Doloremque maxime quibusdam, recusandae
@@ -68,15 +69,32 @@ const AboutPage = () => {
               error quisquam ducimus obcaecati.Doloremque maxime quibusdam,
               recusandae voluptatem error quisquam ducimus obcaecati.
             </p>
+            <aside>
+              <img src={img46} alt='shinobi-view' />
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Assumenda molestiae quod alias ex atque est eveniet, numquam
+                placeat optio deleniti at natus, explicabo praesentium eligendi
+                adipisci itaque nostrum blanditiis laudantium eos fugiat! Illo
+                possimus inventore, sed nobis unde quod, expedita consequuntur
+                modi minus doloremque enim, recusandae illum. Illum, obcaecati
+                quam. Assumenda molestiae quod alias ex atque est eveniet,
+                numquam placeat optio deleniti at natus, explicabo praesentium
+                eligendi adipisci itaque nostrum blanditiis laudantium eos
+                fugiat! Illo possimus inventore, sed nobis unde quod, expedita
+                consequuntur modi minus doloremque enim, recusandae illum.
+                Illum, obcaecati quam.
+              </p>
+            </aside>
           </div>
         </section>
 
-        <section className="container flow">
-          <div className="team-title flex">
+        <section className='container flow'>
+          <div className='team-title flex'>
             <h3>MEET OUR TEAM</h3>
           </div>
 
-          <div className="coaches-wrapper container grid">
+          <div className='coaches-wrapper container grid'>
             {data.teamMembers.map((member) =>
               teamMemberCard(member.id, member.name, member.xp, member.skills)
             )}
