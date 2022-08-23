225c0eac560063958f708b2a67bdb331c1aa7d35
Ion Albu <ialbu.webdev@gmail.com>
2022-08-23 16:36:14 +0100

    insert dynamic opacity in about and classes pages, remove parallax speed from classes page

--- shinobi-frontend/src/Components/AboutPage/AboutPage.jsx
+++ shinobi-frontend/src/Components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import './AboutPage.css';
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import Tilt from 'react-parallax-tilt';
 import data from '../../data.json';
 import img46 from '../../assets/gallery-assets/gallery-media/img46.jpg';
@@ -26,86 +27,98 @@ const teamMemberCard = (id, name, xp, skills) => {
 
 const AboutPage = () => {
   return (
-    <div className='about-page grid'>
-      <div className='page-title' id='about-page'>
-        <h3>About us</h3>
-      </div>
-      <div className='about-page-content shadowed-box'>
-        <section className='container flow'>
-          <div className='container'>
-            <div className='photo-colin'>
-              <Tilt
-                className='parallax-effect-glare-scale'
-                perspective={500}
-                glareEnable={true}
-                glareMaxOpacity={0.5}
-                scale={1.01}
-              >
-                <img
-                  src='https://source.unsplash.com/cVLOqm8sSXc'
-                  loading='lazy'
-                  alt=''
-                />
-              </Tilt>
-            </div>
-
-            <p className='text-dark text-content' id='about-text'>
-              This is Colin Byrne, the founder of Shinobi Academy. Labore
-              consequuntur officia adipisci, dolorum et dolorem neque explicabo
-              aut nostrum rem atque. Doloremque maxime quibusdam, recusandae
-              voluptatem error quisquam ducimus obcaecati. Lorem ipsum dolor sit
-              amet consectetur adipisicing elit. Labore consequuntur officia
-              adipisci, dolorum et dolorem neque explicabo aut nostrum rem
-              atque. Doloremque maxime quibusdam, recusandae voluptatem error
-              quisquam ducimus obcaecati. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Labore consequuntur officia adipisci, dolorum et
-              dolorem neque explicabo aut nostrum rem atque. Doloremque maxime
-              quibusdam, recusandae voluptatem error quisquam ducimus obcaecati.
-              Doloremque maxime quibusdam, recusandae voluptatem error quisquam
-              ducimus obcaecati.Doloremque maxime quibusdam, recusandae
-              voluptatem error quisquam ducimus obcaecati.Doloremque maxime
-              quibusdam, recusandae voluptatem error quisquam ducimus obcaecati.
-              Labore consequuntur officia adipisci, dolorum et dolorem neque
-              explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
-              recusandae voluptatem error quisquam ducimus obcaecati. Doloremque
-              maxime quibusdam, recusandae voluptatem error quisquam ducimus
-              obcaecati. Doloremque maxime quibusdam, recusandae voluptatem
-              error quisquam ducimus obcaecati.Doloremque maxime quibusdam,
-              recusandae voluptatem error quisquam ducimus obcaecati.
-            </p>
-            <aside>
-              <img src={img46} loading='lazy' alt='shinobi-view' />
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Assumenda molestiae quod alias ex atque est eveniet, numquam
-                placeat optio deleniti at natus, explicabo praesentium eligendi
-                adipisci itaque nostrum blanditiis laudantium eos fugiat! Illo
-                possimus inventore, sed nobis unde quod, expedita consequuntur
-                modi minus doloremque enim, recusandae illum. Illum, obcaecati
-                quam. Assumenda molestiae quod alias ex atque est eveniet,
-                numquam placeat optio deleniti at natus, explicabo praesentium
-                eligendi adipisci itaque nostrum blanditiis laudantium eos
-                fugiat! Illo possimus inventore, sed nobis unde quod, expedita
-                consequuntur modi minus doloremque enim, recusandae illum.
-                Illum, obcaecati quam.
-              </p>
-            </aside>
+    <ParallaxProvider>
+      <Parallax opacity={[3, 0]}>
+        <div className='about-page grid'>
+          <div className='page-title' id='about-page'>
+            <h3>About us</h3>
           </div>
-        </section>
+          <div className='about-page-content shadowed-box'>
+            <section className='container flow'>
+              <div className='container'>
+                <div className='photo-colin'>
+                  <Tilt
+                    className='parallax-effect-glare-scale'
+                    perspective={500}
+                    glareEnable={true}
+                    glareMaxOpacity={0.5}
+                    scale={1.01}
+                  >
+                    <img
+                      src='https://source.unsplash.com/cVLOqm8sSXc'
+                      loading='lazy'
+                      alt=''
+                    />
+                  </Tilt>
+                </div>
 
-        <section className='container flow'>
-          <div className='team-title flex'>
-            <h3>MEET OUR TEAM</h3>
-          </div>
+                <p className='text-dark text-content' id='about-text'>
+                  This is Colin Byrne, the founder of Shinobi Academy. Labore
+                  consequuntur officia adipisci, dolorum et dolorem neque
+                  explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
+                  recusandae voluptatem error quisquam ducimus obcaecati. Lorem
+                  ipsum dolor sit amet consectetur adipisicing elit. Labore
+                  consequuntur officia adipisci, dolorum et dolorem neque
+                  explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
+                  recusandae voluptatem error quisquam ducimus obcaecati. Lorem
+                  ipsum dolor sit amet consectetur adipisicing elit. Labore
+                  consequuntur officia adipisci, dolorum et dolorem neque
+                  explicabo aut nostrum rem atque. Doloremque maxime quibusdam,
+                  recusandae voluptatem error quisquam ducimus obcaecati.
+                  Doloremque maxime quibusdam, recusandae voluptatem error
+                  quisquam ducimus obcaecati.Doloremque maxime quibusdam,
+                  recusandae voluptatem error quisquam ducimus
+                  obcaecati.Doloremque maxime quibusdam, recusandae voluptatem
+                  error quisquam ducimus obcaecati. Labore consequuntur officia
+                  adipisci, dolorum et dolorem neque explicabo aut nostrum rem
+                  atque. Doloremque maxime quibusdam, recusandae voluptatem
+                  error quisquam ducimus obcaecati. Doloremque maxime quibusdam,
+                  recusandae voluptatem error quisquam ducimus obcaecati.
+                  Doloremque maxime quibusdam, recusandae voluptatem error
+                  quisquam ducimus obcaecati.Doloremque maxime quibusdam,
+                  recusandae voluptatem error quisquam ducimus obcaecati.
+                </p>
+                <aside>
+                  <img src={img46} loading='lazy' alt='shinobi-view' />
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Assumenda molestiae quod alias ex atque est eveniet, numquam
+                    placeat optio deleniti at natus, explicabo praesentium
+                    eligendi adipisci itaque nostrum blanditiis laudantium eos
+                    fugiat! Illo possimus inventore, sed nobis unde quod,
+                    expedita consequuntur modi minus doloremque enim, recusandae
+                    illum. Illum, obcaecati quam. Assumenda molestiae quod alias
+                    ex atque est eveniet, numquam placeat optio deleniti at
+                    natus, explicabo praesentium eligendi adipisci itaque
+                    nostrum blanditiis laudantium eos fugiat! Illo possimus
+                    inventore, sed nobis unde quod, expedita consequuntur modi
+                    minus doloremque enim, recusandae illum. Illum, obcaecati
+                    quam.
+                  </p>
+                </aside>
+              </div>
+            </section>
 
-          <div className='coaches-wrapper container grid'>
-            {data.teamMembers.map((member) =>
-              teamMemberCard(member.id, member.name, member.xp, member.skills)
-            )}
+            <section className='container flow'>
+              <div className='team-title flex'>
+                <h3>MEET OUR TEAM</h3>
+              </div>
+
+              <div className='coaches-wrapper container grid'>
+                {data.teamMembers.map((member) =>
+                  teamMemberCard(
+                    member.id,
+                    member.name,
+                    member.xp,
+                    member.skills
+                  )
+                )}
+              </div>
+            </section>
           </div>
-        </section>
-      </div>
-    </div>
+        </div>
+      </Parallax>
+    </ParallaxProvider>
   );
 };
 
--- shinobi-frontend/src/Components/ClassesPage/ClassesPage.jsx
+++ shinobi-frontend/src/Components/ClassesPage/ClassesPage.jsx
@@ -1,76 +1,88 @@
 import './ClassesPage.css';
-import { Parallax } from 'react-scroll-parallax';
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import { Link } from 'react-router-dom';
 
 const ClassesPage = () => {
   return (
     <div className='classes-page grid'>
-      <div className='flow'>
-        <div className='page-title' id='classes-page'>
-          <h3 id='classes-page'>Classes</h3>
-        </div>
-
-        <div className='classes-content shadowed-box grid text-silver'>
-          <div className='classes class-s'>
-            <div className='classes-name'>
-              <h3>MMA <br /> Box <br /> Muay-Thai</h3>
+      <ParallaxProvider>
+        <Parallax opacity={[3, 0]}>
+          <div className='flow'>
+            <div className='page-title' id='classes-page'>
+              <h3 id='classes-page'>Classes</h3>
             </div>
 
-            <Parallax speed={-13} opacity={[0, 2]}>
-              <p className='classes-text-right'>
-                our classes Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Corporis, nesciunt totam nostrum provident quis placeat
-                minus sunt architecto assumenda consequuntur?
-              </p>
-            </Parallax>
-          </div>
+            <div className='classes-content shadowed-box grid text-silver'>
+              <div className='classes class-s'>
+                <div className='classes-name'>
+                  <h3>
+                    MMA <br /> Box <br /> Muay-Thai
+                  </h3>
+                </div>
 
-          <div className='classes class-jj'>
-            <div className='classes-name'>
-              <h3>BJJ <br /> Grappling</h3>
+                <Parallax opacity={[0, 2]}>
+                  <p className='classes-text-right'>
+                    our classes Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Corporis, nesciunt totam nostrum provident
+                    quis placeat minus sunt architecto assumenda consequuntur?
+                  </p>
+                </Parallax>
+              </div>
+
+              <div className='classes class-jj'>
+                <div className='classes-name'>
+                  <h3>
+                    BJJ <br /> Grappling
+                  </h3>
+                </div>
+                <Parallax opacity={[0, 2]}>
+                  <p className='classes-text-left'>
+                    our classes Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Corporis, nesciunt totam nostrum provident
+                    quis placeat minus sunt architecto assumenda consequuntur?
+                  </p>
+                </Parallax>
+              </div>
+
+              <div className='classes class-w'>
+                <div className='classes-name'>
+                  <h3>
+                    Wrestling <br /> Judo
+                  </h3>
+                </div>
+                <Parallax opacity={[0, 2]}>
+                  <p className='classes-text-right'>
+                    our classes Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Corporis, nesciunt totam nostrum provident
+                    quis placeat minus sunt architecto assumenda consequuntur?
+                  </p>
+                </Parallax>
+              </div>
+
+              <div className='classes class-c'>
+                <div className='classes-name'>
+                  <h3>
+                    Strength <br /> Condition
+                  </h3>
+                </div>
+                <Parallax opacity={[0, 2]}>
+                  <p className='classes-text-left'>
+                    our classes Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Corporis, nesciunt totam nostrum provident
+                    quis placeat minus sunt architecto assumenda consequuntur?
+                  </p>
+                </Parallax>
+              </div>
+
+              <div className='schedule-button'>
+                <button className='button-large'>
+                  <Link to='/schedule'>View schedule</Link>
+                </button>
+              </div>
             </div>
-            <Parallax speed={-16} opacity={[0, 2]}>
-              <p className='classes-text-left'>
-                our classes Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Corporis, nesciunt totam nostrum provident quis placeat
-                minus sunt architecto assumenda consequuntur?
-              </p>
-            </Parallax>
           </div>
-
-          <div className='classes class-w'>
-            <div className='classes-name'>
-              <h3>Wrestling <br /> Judo</h3>
-            </div>
-            <Parallax speed={-19} opacity={[0, 2]}>
-              <p className='classes-text-right'>
-                our classes Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Corporis, nesciunt totam nostrum provident quis placeat
-                minus sunt architecto assumenda consequuntur?
-              </p>
-            </Parallax>
-          </div>
-
-          <div className='classes class-c'>
-            <div className='classes-name'>
-              <h3>Strength <br /> Condition</h3>
-            </div>
-            <Parallax speed={-22} opacity={[0, 2]}>
-              <p className='classes-text-left'>
-                our classes Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Corporis, nesciunt totam nostrum provident quis placeat
-                minus sunt architecto assumenda consequuntur?
-              </p>
-            </Parallax>
-          </div>
-
-          <div className='schedule-button'>
-            <button className='button-large'>
-              <Link to='/schedule'>View schedule</Link>
-            </button>
-          </div>
-        </div>
-      </div>
+        </Parallax>
+      </ParallaxProvider>
     </div>
   );
 };
