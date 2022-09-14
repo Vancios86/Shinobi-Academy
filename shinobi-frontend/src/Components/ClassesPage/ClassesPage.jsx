f9c8e76089a173f7b6f4ab340466d63a526a3a5d
Ion Albu <ialbu.webdev@gmail.com>
2022-09-14 12:06:35 +0100

    change classes background image url from static to GDrive, change parallax behaviour, improve page performance

--- shinobi-frontend/src/Components/ClassesPage/ClassesPage.jsx
+++ shinobi-frontend/src/Components/ClassesPage/ClassesPage.jsx
@@ -1,88 +1,84 @@
 import './ClassesPage.css';
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import { Parallax } from 'react-scroll-parallax';
 import { Link } from 'react-router-dom';
 
 const ClassesPage = () => {
   return (
     <div className='classes-page grid'>
-      <ParallaxProvider>
-        <Parallax opacity={[3, 0]}>
-          <div className='flow'>
-            <div className='page-title' id='classes-page'>
-              <h3 id='classes-page'>Classes</h3>
+      <div className='flow'>
+        <div className='page-title' id='classes-page'>
+          <h3 id='classes-page'>Classes</h3>
+        </div>
+
+        <div className='classes-content shadowed-box grid text-silver'>
+          <div className='classes class-s'>
+            <div className='classes-name'>
+              <h3>
+                MMA <br /> Box <br /> Muay-Thai
+              </h3>
             </div>
 
-            <div className='classes-content shadowed-box grid text-silver'>
-              <div className='classes class-s'>
-                <div className='classes-name'>
-                  <h3>
-                    MMA <br /> Box <br /> Muay-Thai
-                  </h3>
-                </div>
-
-                <Parallax opacity={[0, 2]}>
-                  <p className='classes-text-right'>
-                    our classes Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Corporis, nesciunt totam nostrum provident
-                    quis placeat minus sunt architecto assumenda consequuntur?
-                  </p>
-                </Parallax>
-              </div>
-
-              <div className='classes class-jj'>
-                <div className='classes-name'>
-                  <h3>
-                    BJJ <br /> Grappling
-                  </h3>
-                </div>
-                <Parallax opacity={[0, 2]}>
-                  <p className='classes-text-left'>
-                    our classes Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Corporis, nesciunt totam nostrum provident
-                    quis placeat minus sunt architecto assumenda consequuntur?
-                  </p>
-                </Parallax>
-              </div>
-
-              <div className='classes class-w'>
-                <div className='classes-name'>
-                  <h3>
-                    Wrestling <br /> Judo
-                  </h3>
-                </div>
-                <Parallax opacity={[0, 2]}>
-                  <p className='classes-text-right'>
-                    our classes Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Corporis, nesciunt totam nostrum provident
-                    quis placeat minus sunt architecto assumenda consequuntur?
-                  </p>
-                </Parallax>
-              </div>
-
-              <div className='classes class-c'>
-                <div className='classes-name'>
-                  <h3>
-                    Strength <br /> Condition
-                  </h3>
-                </div>
-                <Parallax opacity={[0, 2]}>
-                  <p className='classes-text-left'>
-                    our classes Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Corporis, nesciunt totam nostrum provident
-                    quis placeat minus sunt architecto assumenda consequuntur?
-                  </p>
-                </Parallax>
-              </div>
-
-              <div className='schedule-button'>
-                <button className='button-large'>
-                  <Link to='/schedule'>View schedule</Link>
-                </button>
-              </div>
-            </div>
+            <Parallax speed={9}>
+              <p className='classes-text-right'>
+                our classes Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Corporis, nesciunt totam nostrum provident quis placeat
+                minus sunt architecto assumenda consequuntur?
+              </p>
+            </Parallax>
           </div>
-        </Parallax>
-      </ParallaxProvider>
+
+          <div className='classes class-jj'>
+            <div className='classes-name'>
+              <h3>
+                BJJ <br /> Grappling
+              </h3>
+            </div>
+            <Parallax speed={11}>
+              <p className='classes-text-left'>
+                our classes Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Corporis, nesciunt totam nostrum provident quis placeat
+                minus sunt architecto assumenda consequuntur?
+              </p>
+            </Parallax>
+          </div>
+
+          <div className='classes class-w'>
+            <div className='classes-name'>
+              <h3>
+                Wrestling <br /> Judo
+              </h3>
+            </div>
+            <Parallax speed={13}>
+              <p className='classes-text-right'>
+                our classes Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Corporis, nesciunt totam nostrum provident quis placeat
+                minus sunt architecto assumenda consequuntur?
+              </p>
+            </Parallax>
+          </div>
+
+          <div className='classes class-c'>
+            <div className='classes-name'>
+              <h3>
+                Strength <br /> Condition
+              </h3>
+            </div>
+            <Parallax speed={15}>
+              <p className='classes-text-left'>
+                our classes Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Corporis, nesciunt totam nostrum provident quis placeat
+                minus sunt architecto assumenda consequuntur?
+              </p>
+            </Parallax>
+          </div>
+
+          <div className='schedule-button'>
+            <button className='button-large'>
+              <Link to='/schedule'>View schedule</Link>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
